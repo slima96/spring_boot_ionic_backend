21995edadb2b36f454d6af1ad18253b8a06dd87c
Sandro Lima <sandro.lima0016@gmail.com>
2020-10-01 16:18:00 -0300

    Testando primeira classe de dominio

--- src/main/java/br/com/viatekbrasil/industrial/resources/EmpresaResource.java
+++ src/main/java/br/com/viatekbrasil/industrial/resources/EmpresaResource.java
@@ -1,15 +1,29 @@
 package br.com.viatekbrasil.industrial.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.viatekbrasil.industrial.domain.Empresa;
 
 @RestController
 @RequestMapping(value="/empresas")
 public class EmpresaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionado";
-	}
+	public List<Empresa> listar() {
+		
+		Empresa emp1 = new Empresa(1,"Viatek");
+		Empresa emp2 = new Empresa(2, "Viamed");
+		
+		List<Empresa> lista = new ArrayList<>();
+		
+		lista.addAll(Arrays.asList(emp1, emp2));
+		
+		return lista;
+	}	
 }
