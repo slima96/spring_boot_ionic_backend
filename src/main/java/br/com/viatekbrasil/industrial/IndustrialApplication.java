32830346ff6925f5e8f91a51a93babbd6674624c
Sandro Lima <sandro.lima0016@gmail.com>
2020-10-01 15:50:13 -0300

    Projeto criado

---
+++ src/main/java/br/com/viatekbrasil/industrial/IndustrialApplication.java
@@ -0,0 +1,13 @@
+package br.com.viatekbrasil.industrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustrialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/viatekbrasil/industrial/IndustrialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viatekbrasil.industrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
