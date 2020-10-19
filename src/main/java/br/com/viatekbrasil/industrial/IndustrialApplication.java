324ff201ed8f3e8c82661e112b15511e9f379b77
Sandro Lima <sandro.lima0016@gmail.com>
2020-10-19 12:12:03 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/br/com/viatekbrasil/industrial/IndustrialApplication.java
+++ src/main/java/br/com/viatekbrasil/industrial/IndustrialApplication.java
@@ -1,18 +1,25 @@
 package br.com.viatekbrasil.industrial;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.viatekbrasil.industrial.services.S3Service;
+
 @SpringBootApplication
 public class IndustrialApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(IndustrialApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\temp\\modelo.jpg");
 	}
 
 }
