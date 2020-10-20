6e55cb42f2226c54b3c779efa9021786d6659220
Sandro Lima <sandro.lima0016@gmail.com>
2020-10-20 14:08:36 -0300

    Liberando CORS para PUT e DELETE

--- src/main/java/br/com/viatekbrasil/industrial/config/SecurityConfig.java
+++ src/main/java/br/com/viatekbrasil/industrial/config/SecurityConfig.java
@@ -75,8 +75,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Bean
 	  CorsConfigurationSource corsConfigurationSource() {
-	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+	    CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+	    configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
 	    return source;
 	  }
 	
--- src/main/java/br/com/viatekbrasil/industrial/services/DBService.java
+++ src/main/java/br/com/viatekbrasil/industrial/services/DBService.java
@@ -56,7 +56,6 @@ public class DBService {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 
-	
 	public void instatiateTestDatabase() {
 		Empresa viatek = new Empresa(null, "Viatek");
 		Empresa viamed = new Empresa(null, "Viamed");
@@ -82,12 +81,57 @@ public class DBService {
 		Produto balde =  new Produto(null, "10001", "Balde para concreto", 35, 1, 3.0, linha1, viatek);
 		Produto pivoMondial = new Produto(null, "1020-01", "Pivo mondial", 20, 4, 1.5, linha2, viamed);
 		Produto acoplamento =  new Produto(null, "102030", "Acoplamento JCS", 20, 6, 1.5, linha3, gplast);
+		Produto p1 =  new Produto(null, "10002", "Produto 1", 35, 1, 3.0, linha1, viatek);
+		Produto p2 =  new Produto(null, "10003", "Produto 2", 35, 1, 3.0, linha1, viatek);
+		Produto p3 =  new Produto(null, "10004", "Produto 3", 35, 1, 3.0, linha1, viatek);
+		Produto p4 =  new Produto(null, "10005", "Produto 4", 35, 1, 3.0, linha1, viatek);
+		Produto p5 =  new Produto(null, "10006", "Produto 5", 35, 1, 3.0, linha1, viatek);
+		Produto p6 =  new Produto(null, "10007", "Produto 6", 35, 1, 3.0, linha1, viatek);
+		Produto p7 =  new Produto(null, "10008", "Produto 7", 35, 1, 3.0, linha1, viatek);
+		Produto p8 =  new Produto(null, "10009", "Produto 8", 35, 1, 3.0, linha1, viatek);
+		Produto p9 =  new Produto(null, "10010", "Produto 9", 35, 1, 3.0, linha1, viatek);
+		Produto p10 =  new Produto(null, "10011", "Produto 10", 35, 1, 3.0, linha1, viatek);
+		Produto p11 =  new Produto(null, "10012", "Produto 11", 35, 1, 3.0, linha1, viatek);
+		Produto p12 =  new Produto(null, "10013", "Produto 12", 35, 1, 3.0, linha1, viatek);
+		Produto p13 =  new Produto(null, "10014", "Produto 13", 35, 1, 3.0, linha1, viatek);
+		Produto p14 =  new Produto(null, "10015", "Produto 14", 35, 1, 3.0, linha1, viatek);
+		Produto p15 =  new Produto(null, "10016", "Produto 15", 35, 1, 3.0, linha1, viatek);
+		Produto p16 =  new Produto(null, "10017", "Produto 16", 35, 1, 3.0, linha1, viatek);
+		Produto p17 =  new Produto(null, "10018", "Produto 17", 35, 1, 3.0, linha1, viatek);
+		Produto p18 =  new Produto(null, "10019", "Produto 18", 35, 1, 3.0, linha1, viatek);
+		Produto p19 =  new Produto(null, "10020", "Produto 19", 35, 1, 3.0, linha1, viatek);
+		Produto p20 =  new Produto(null, "10021", "Produto 20", 35, 1, 3.0, linha1, viatek);
+		Produto p21 =  new Produto(null, "10022", "Produto 21", 35, 1, 3.0, linha1, viatek);
+		Produto p22 =  new Produto(null, "10023", "Produto 22", 35, 1, 3.0, linha1, viatek);
+		Produto p23 =  new Produto(null, "10024", "Produto 23", 35, 1, 3.0, linha1, viatek);
+		Produto p24 =  new Produto(null, "10025", "Produto 24", 35, 1, 3.0, linha1, viatek);
+		Produto p25 =  new Produto(null, "10026", "Produto 25", 35, 1, 3.0, linha1, viatek);
+		Produto p26 =  new Produto(null, "10027", "Produto 26", 35, 1, 3.0, linha1, viatek);
+		Produto p27 =  new Produto(null, "10028", "Produto 27", 35, 1, 3.0, linha1, viatek);
+		Produto p28 =  new Produto(null, "10029", "Produto 28", 35, 1, 3.0, linha1, viatek);
+		Produto p29 =  new Produto(null, "10030", "Produto 29", 35, 1, 3.0, linha1, viatek);
+		Produto p30 =  new Produto(null, "10031", "Produto 30", 35, 1, 3.0, linha1, viatek);
+		Produto p31 =  new Produto(null, "10032", "Produto 31", 35, 1, 3.0, linha1, viatek);
+		Produto p32 =  new Produto(null, "10033", "Produto 32", 35, 1, 3.0, linha1, viatek);
+		Produto p33 =  new Produto(null, "10034", "Produto 33", 35, 1, 3.0, linha1, viatek);
+		Produto p34 =  new Produto(null, "10035", "Produto 34", 35, 1, 3.0, linha1, viatek);
+		Produto p35 =  new Produto(null, "10036", "Produto 35", 35, 1, 3.0, linha1, viatek);
+		Produto p36 =  new Produto(null, "10037", "Produto 36", 35, 1, 3.0, linha1, viatek);
+		Produto p37 =  new Produto(null, "10038", "Produto 37", 35, 1, 3.0, linha1, viatek);
+		Produto p38 =  new Produto(null, "10039", "Produto 38", 35, 1, 3.0, linha1, viatek);
+		Produto p39 =  new Produto(null, "10040", "Produto 39", 35, 1, 3.0, linha1, viatek);
+		Produto p40 =  new Produto(null, "10041", "Produto 40", 35, 1, 3.0, linha1, viatek);
 		
-		viatek.getProdutos().addAll(Arrays.asList(balde));
+
+		viatek.getProdutos().addAll(Arrays.asList(balde, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14,
+													p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30,
+													p31, p32, p33, p34, p35, p36, p37, p38, p39, p40));
 		viamed.getProdutos().addAll(Arrays.asList(pivoMondial));
 		gplast.getProdutos().addAll(Arrays.asList(acoplamento));
 		
-		linha1.getProdutos().addAll(Arrays.asList(balde));
+		linha1.getProdutos().addAll(Arrays.asList(balde, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14,
+													p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30,
+													p31, p32, p33, p34, p35, p36, p37, p38, p39, p40));
 		linha2.getProdutos().addAll(Arrays.asList(pivoMondial));
 		linha3.getProdutos().addAll(Arrays.asList(acoplamento));
 		
@@ -112,7 +156,9 @@ public class DBService {
 		linhaRepository.saveAll(Arrays.asList(linha1, linha2, linha3));
 		equipamentoRepository.saveAll(Arrays.asList(equipamento));
 		turnoRepository.saveAll(Arrays.asList(turno1, turno2, turno3));
-		produtoRepository.saveAll(Arrays.asList(balde, pivoMondial, acoplamento));
+		produtoRepository.saveAll(Arrays.asList(balde, pivoMondial, acoplamento, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14,
+													p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30,
+													p31, p32, p33, p34, p35, p36, p37, p38, p39, p40));
 		movimentoRepository.saveAll(Arrays.asList(movimento));
 		movimentoDetalheRepository.saveAll(Arrays.asList(mov1, mov2, mov3));
 	}
