9c07bd81dd8fc0f2fc5a4427cecf22a89e3aefbc
Alexsandro Leão Lima <sandro.lima@viatek.local>
2020-10-19 21:01:09 -0300

    Tratando excecoes adequadamente

--- src/main/java/br/com/viatekbrasil/industrial/services/S3Service.java
+++ src/main/java/br/com/viatekbrasil/industrial/services/S3Service.java
@@ -14,6 +14,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
+import br.com.viatekbrasil.industrial.services.exceptions.FileException;
+
 @Service
 public class S3Service {
 
@@ -34,7 +36,7 @@ public class S3Service {
 
 			return uploadFile(is, fileName, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("Erro de IO: " + e.getMessage());
+			throw new FileException("Erro de IO: " + e.getMessage());
 		}
 
 	}
@@ -50,7 +52,7 @@ public class S3Service {
 
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 }
